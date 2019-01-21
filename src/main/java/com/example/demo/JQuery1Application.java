dae74403a0347134bbc4bfd4b194006b4e859653
labixiaoxinhoho <790082523@qq.com>
2019-01-21 16:36:32 +0800

    初始项目

---
+++ src/main/java/com/example/demo/JQuery1Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JQuery1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JQuery1Application.class, args);
+	}
+
+}
+
